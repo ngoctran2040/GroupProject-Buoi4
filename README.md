4d65ed088eb1e1358ee6bff0cef0397904684f9c
NgocTran <ngoctran2040@gmail.com>
2025-10-12 17:16:58 +0700

    Frontend React

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# GroupProject-Buoi4
+THỰC HÀNH NHÓM: Node.js +React+MongoDB+GitHub
+Vai trò thành viên:
+Nguyễn Ngọc Trúc Phương (sinh viên 1): Backend (Node.js+Express)
+Nguyễn Ngọc Trân (sinh viên 2): Frontend (React)
+Dương Trần Phúc (sinh viên 3): Database (MongoDB)
+
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quản lý User</h1>
+      <AddUser />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
